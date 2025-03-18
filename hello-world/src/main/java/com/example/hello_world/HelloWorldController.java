ef76e116f1045740e6bd022130b9c917a072b4ad
o-ta-m <misako.ota@mcomp.co.jp>
2025-03-18 11:28:52 +0900

    add:new project

---
+++ hello-world/src/main/java/com/example/hello_world/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.hello_world;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloWorldController {
+    
+    @GetMapping("/Hello")
+    public String helloWorld() {
+        return new String("Hello World!!");
+    }
+    
+}
